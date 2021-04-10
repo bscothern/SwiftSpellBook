669033886dbc32df5a96685c3de8a1f419a0d9fb
Braden Scothern <BradenScothern@gmail.com>
2021-04-09 23:09:52 -0600

    Added clamping to comparable values

---
+++ Sources/Extensions/Comparable.swift
@@ -0,0 +1,18 @@
+//
+//  Comparable.swift
+//  SwiftSpellBook
+//
+//  Created by Braden Scothern on 4/9/21.
+//  Copyright © 2020-2021 Braden Scothern. All rights reserved.
+//
+
+extension Comparable {
+    @inlinable
+    public mutating func clamp(to range: ClosedRange<Self>) {
+        if self < range.lowerBound {
+            self = range.lowerBound
+        } else if self > range.upperBound {
+            self = range.upperBound
+        }
+    }
+}
